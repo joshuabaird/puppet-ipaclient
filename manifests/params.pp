2adad6723e580ffeff56d2208a6a42c176ea2dff
Stephen Benjamin <stephen@bitbin.de>
2014-03-22 01:31:10 +0100

    Cleaned up params class

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,40 +1,18 @@
-# IPAclient Default Parameters
+# IPAclient Default Settings
 class ipaclient::params {
-  # Whether to manual register or not (if not, IPA will look for  DNS SRV records)
+
   $manual_register = false
-
-  # Automatically make user home directories
   $mkhomedir       = true
-
-  # Join user and password
   $join_user       = ''
   $join_pw         = 'UNSET'
-
-  # An enrollment host is an actual IPA server to register to
-  $enrollment_host = ''
-
-  # IPA server is used for manual configruation, this is
-  # normally the same as the enrollment host, but it could
-  # also be a load-balanced VIP
-  $ipa_server      = ''
-
-  # IPA domain, e.g. example.com
-  $ipa_domain      = ''
-
-  # IPA realm, e.g. EXAMPLE.COM
   $ipa_realm       = 'UNSET'
-
-  # List of IPA servers + replicas
+  $enrollment_host = ''
+  $ipa_server      = ''
+  $ipa_domain      = ''
   $replicas        = []
-
-  # LDAP D.N., e.g. dc=example,dc=com
   $domain_dn       = ''
-
-  # Enable sudo management, and set password for the sudo bind user
   $enable_sudo     = false
   $sudo_bindpw     = ''
-
-  # Installer location
   $ipa_installer = '/usr/sbin/ipa-client-install'
 
   # Name of IPA package to install
